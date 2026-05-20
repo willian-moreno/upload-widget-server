599dbb5d4e262ca4044fc5bfdd014ddf88438108
Willian Alves Moreno <willianam@uninove.br>
2026-05-20 18:44:21 -0300

    feat: Documentação com Swagger

--- src/infra/http/routes/upload-image.ts
+++ src/infra/http/routes/upload-image.ts
@@ -1,10 +1,29 @@
 import type { FastifyInstance } from 'fastify'
 import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
+import z from 'zod'
 
 export const uploadImageRoute: FastifyPluginAsyncZod = async (
   server: FastifyInstance
 ) => {
-  server.post('/uploads', () => {
-    return 'Hello World ✨'
-  })
+  server.post(
+    '/uploads',
+    {
+      schema: {
+        summary: 'Upload an image',
+        body: z.object({
+          name: z.string(),
+          password: z.string().optional(),
+        }),
+        response: {
+          201: z.object({ uploadId: z.string() }),
+          409: z.object({
+            message: z.string().describe('Upload already exists'),
+          }),
+        },
+      },
+    },
+    async (_, reply) => {
+      return await reply.status(201).send({ uploadId: crypto.randomUUID() })
+    }
+  )
 }
--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,7 +1,11 @@
 import { fastifyCors } from '@fastify/cors'
+import fastifyMultipart from '@fastify/multipart'
+import fastifySwagger from '@fastify/swagger'
+import fastifySwaggerUi from '@fastify/swagger-ui'
 import { fastify } from 'fastify'
 import {
   hasZodFastifySchemaValidationErrors,
+  jsonSchemaTransform,
   serializerCompiler,
   validatorCompiler,
 } from 'fastify-type-provider-zod'
@@ -26,6 +30,22 @@ server.setErrorHandler((error, _, reply) => {
 })
 
 server.register(fastifyCors, { origin: '*' })
+
+server.register(fastifyMultipart)
+
+server.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Image Uploader API',
+      version: '1.0.0',
+    },
+  },
+  transform: jsonSchemaTransform,
+})
+
+server.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+})
 
 server.register(uploadImageRoute)
 
